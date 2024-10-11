e72107b548bbff982febf4ec51da836a860f2e88
Globbox <gigabit1812@gmail.com>
2024-10-11 20:16:01 +1000

    feat: API для ответов

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -2,10 +2,10 @@ from pathlib import Path
 import os
 
 
-LOG_DIR = Path(__file__).resolve().parent.joinpath('logs')
+LOG_DIR = Path(__file__).resolve().parent.parent.joinpath('logs')
 
-bind = '0.0.0.0:8000'
-workers = int(os.getenv('GUNICORN_WORKERS_COUNT', os.cpu_count()))
+bind = '0.0.0.0:4000'
+workers = int(os.getenv('GUNICORN_WORKERS_COUNT', os.cpu_count() + 1))
 proc_name = 'trainer_backend'
 
 errorlog = f'{LOG_DIR}/gunicorn.log'
